79fdf1c4ece0768777d8c37fe16235cca189fdea
Dhruv Vyas <dhruvvyas8866@gmail.com>
2024-09-28 14:19:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,6 @@ def predict():
     
     return jsonify({'result': winning_percentage})
 
-    @app.route('/test', methods=['GET'])
-       def test():
-           return jsonify({"message": "API is working"}), 200
+   
 # if __name__ == '__main__':
     app.run()
